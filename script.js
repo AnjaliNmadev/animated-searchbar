15b744107b44fec8db23724fc71e16b9c381fbbd
AnjaliNmadev <versaanjali04@gmail.com>
2024-11-02 16:10:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let searchbtn = document.querySelector(".search-btn")
+let conatiner= document.querySelector(".conatiner")
+searchbtn.addEventListener("click", ()=>
+{
+    conatiner.classList.toggle("active");
+   
+}
+)
